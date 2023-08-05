3fce73a79b4cc24f1be6ee410375c4d21010192c
Nahuel Cabrera Zembrunski <113146310+NahuelCabreraZ@users.noreply.github.com>
2023-08-05 15:37:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { CargaAutos } from './components/CargaAutos'
+import { NavBarTest } from './components/NavBarTest'
+import { NavBar } from './components/NavBar'
+import { NavBarTestMain } from './components/NavBarTestMain'
+import { HomeTest } from './components/HomeTest'
+import { Login } from './components/Login'
+import { CargaPlan } from './components/QuieroVenderMiPlan'
+import { Adjudicados } from './components/Adjudicados'
+import  Planes  from './components/Planes'
+import { useState, useEffect } from 'react'
+import TermsAndConditions from './components/TerminosYCondiciones'
+import { Usuarios } from './components/endpoints/edits'
+import { EditUsuario } from './components/endpoints/EditUsuario'
+
+function App() {
+
+  const [sesion, setSesion] = useState('');
+  const [user, setUser] = useState('')
+
+        
+        useEffect(()=>{
+          const sesionLogeada = JSON.parse(localStorage.getItem('token'))
+          const userRol = JSON.parse(localStorage.getItem('saveRol'))
+          if(sesionLogeada){
+            setSesion(sesionLogeada)
+            setUser(userRol)
+          }  
+        },[])
+
+        
+  return (
+    <>
+        { !sesion  ? (
+          <>
+          <div><NavBar/></div>
+          </>
+        ) : user === 'admin' ? (
+          <div><NavBarTestMain/></div>
+        ) : (
+          <div><NavBarTest/></div>
+        )}
+            
+      {/* <NavBarTest/> */}
+      <Routes>
+      <Route path="/cargaAutos" element ={<CargaAutos/>}></Route>
+      <Route path='/' element ={<HomeTest/>}></Route>
+      <Route path='/login' element ={<Login/>}></Route>
+      <Route path='/cargaPlan' element ={<CargaPlan/>}></Route>
+      <Route path='/adjudicados' element ={<Adjudicados/>}></Route>
+      <Route path='/planes' element ={<Planes />}></Route>
+      <Route path='/terms' element={<TermsAndConditions/>}></Route>
+      <Route path='/usuario' element={<Usuarios/>}></Route>
+      <Route path='/edit_usuarios/:id' element={<EditUsuario/>}></Route>
+      </Routes>
+    </>
+
+  )
+}
+export default App
---
+++ src/components/NavBarTestMain.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+
+export function NavBarTestMain () {
+  const [sesion, setSesion] = useState('')
+
+    const Logout = async(event)=>{
+
+            setSesion('')
+            window.localStorage.removeItem('token');
+            window.location.reload(true);
+        }
+
+        setTimeout(Logout, 1200000);
+
+    return (
+        <>
+    <Navbar className="barra" variant="dark" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-auto" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link style={{color:"white"}} href='/'>Inicio</Nav.Link>
+          <Nav.Link style={{color:"aquamarine"}} href="/planes">Planes Avanzados</Nav.Link>
+          {/* <Nav.Link style={{color:"white"}} href="#">Adjudicados</Nav.Link> */}
+          <Nav.Link style={{color:"aquamarine"}} href="/cargaPlan">¡Quiero vender mi plan!</Nav.Link>
+          <Nav.Link style={{color:"white"}} href="/cargaAutos">¡Subí tu auto!</Nav.Link>
+          <Nav.Link style={{color:"aquamarine"}} href="/usuario">Usuarios</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+            <div className="logo">
+            <img src="/img/logo.png" width="120px" height="120px" alt="logo" />
+            </div>
+            <Nav.Link onClick={Logout} className="login" href="#"><img src="/img/logout.png" width="60px" height="50px" alt="user" /></Nav.Link>
+    </Navbar>
+
+        </>
+    )
+}
