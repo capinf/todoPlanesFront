4dd0c58283f39d52d39df819bb997bdcf84e272f
Nahuel Cabrera Zembrunski <113146310+NahuelCabreraZ@users.noreply.github.com>
2023-08-06 17:55:31 -0300

    se sacó el proxy porque no está trayendo las imagenes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/uploads': 'https://todoplanesback.onrender.com'  // Cambia la URL del servidor backend si es diferente
-    }
-  }
 })
