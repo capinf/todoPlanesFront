70f4508a8934b981acbcd7dfc7cdb4e27fdf1c7b
Nahuel Cabrera Zembrunski <113146310+NahuelCabreraZ@users.noreply.github.com>
2023-08-07 15:21:12 -0300

    vite sin proxy vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    server: {
-    proxy: {
-      '/uploads': 'https://todoplanesback.onrender.com'  // Cambia la URL del servidor backend si es diferente
-    }
-  }
+  plugins: [react()]
 })
+
+
+  //   server: {
+  //   proxy: {
+  //     '/uploads': 'https://todoplanesback.onrender.com'  // Cambia la URL del servidor backend si es diferente
+  //   }
+  // }
