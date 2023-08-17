11866b4287147c02e491b4539ff7a07a561fff5d
¨capinf¨ <capinf@gmail.com>
2023-08-16 21:03:57 -0300

    Changing Botones

--- src/components/edits.jsx
+++ src/components/edits.jsx
@@ -172,9 +172,8 @@ export function Usuarios(){
     //BOTONES//
 
     const bajaUsuario  = async(id)=>{
-       // console.log('el usuario que vamos a dar de baja es el',id)
 
-        const user = await API.BajaUsuario(id)
+        const user = await API.bajaUsuario(id)
         if(user.status){
             
             setmensajeError(user.mensaje)
@@ -192,7 +191,7 @@ export function Usuarios(){
     }
 
     const altaUsuario = async(id)=>{
-        const user = await API.AltaUsuario(id)
+        const user = await API.altaUsuario(id)
         if(user.status){
             setmensajeSuccess(user.mensaje)
             setTimeout(()=>{
@@ -238,34 +237,38 @@ export function Usuarios(){
             
                 <table className="table text-white">
                     <thead>
-                    <Link to = {'/'}role="button" className="btn btn-outline-primary">Agregar Cliente</Link>
                         <tr>
+                            <th scope="col">Id</th>
                             <th scope="col">Username</th>
                             <th scope="col">Apellido y Nombre</th>
                             <th scope="col">Telefono</th>
                             <th scope="col">Estado</th>
-                            <th scope="col">Acciones</th>
+                            <th scope="col">Rol</th>
                         </tr>
                     </thead>
                     <tbody>
                         {usuario.map((usuarios)=>(
-                        <tr  key={usuarios.username}>
-                            <td scope="row">{usuarios.username}</td>
+                        <tr  key={usuarios.id}>
+                            <td scope="row">{usuarios.id}</td>
                             <td>{usuarios.username}</td>
                             <td>{usuarios.apellido_nombre}</td>
                             <td>{usuarios.telefono}</td>
+                            <td>{usuarios.estado}</td>
+                            <td>{usuarios.rol}</td>
+
+
                             <td>{usuarios.estado=='A'?'Activo':'Baja'}</td>
                             <td>
                             <div className="btn-group" role="group" aria-label="Basic example">
                             { (usuarios.estado=='B')? 
 
-                                <button onClick={() =>altaUsuario(usuarios.username,'A')} type="button" className="btn btn-outline-primary">Alta</button>
+                                <button onClick={() =>altaUsuario(usuarios.id,'A')} type="button" className="btn btn-outline-primary">Alta</button>
                                 :
                                 <>
 
                                
-                                <button onClick={() => bajaUsuario(usuarios.username,'B')} type="button" className="btn btn-outline-danger">Baja</button>
-                                <Link name="" id="" className="btn btn-outline-secondary" to={`/edit_clientes/${usuarios.username}`} role="button">Editar </Link>
+                                <button onClick={() => bajaUsuario(usuarios.id,'B')} type="button" className="btn btn-outline-danger">Baja</button>
+                                <Link name="" id="" className="btn btn-outline-secondary" to={`/edit_clientes/${usuarios.id}`} role="button">Editar </Link>
                                 </>
                             }
 
@@ -283,6 +286,8 @@ export function Usuarios(){
 
     )  
 }
+
+
 
 /// editar usuario ///
 
