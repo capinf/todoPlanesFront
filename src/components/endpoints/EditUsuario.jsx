ede2106ea13c825b38ee074911f51db085537ac2
GermanMoros <germanmoros11@gmail.com>
2023-08-23 19:54:15 -0300

    asd

--- src/components/endpoints/EditUsuario.jsx
+++ src/components/endpoints/EditUsuario.jsx
@@ -34,7 +34,7 @@ export function EditUsuario(){
           console.log('los datos enviados son', datos);
   
           if (datos && datos.length > 0) {
-              const usuario = datos(id);
+              const usuario = datos[id];
               console.log('el nombre de usuario es', usuario.username);
   
               setusername(usuario.username);
